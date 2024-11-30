871f1d091342c7de98843fa7c039393e781c4177
indroji-mondal <indro.cse.bu@gmail.com>
2024-11-30 22:20:18 +0600

    Updated index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // const database = client.db("insertDB");
     // const haiku = database.collection("haiku");
    
@@ -52,12 +52,12 @@ async function run() {
         const result = await ordersCollections.insertOne(newOrders);
         res.send(result);
     })
-    app.get('/orders/:id', async(req, res)=>{
-       const id = req.params.id;
-       const query = {_id: new ObjectId(id)};
-       const result = await ordersCollections.findOne(query);
-       res.send(result);
-    })
+    // app.get('/orders/:id', async(req, res)=>{
+    //    const id = req.params.id;
+    //    const query = {_id: new ObjectId(id)};
+    //    const result = await ordersCollections.findOne(query);
+    //    res.send(result);
+    // })
     app.delete('/orders/:id', async(req, res)=>{
       const id = req.params.id;
       const query = {_id: new ObjectId(id)};
@@ -66,7 +66,7 @@ async function run() {
    })
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
